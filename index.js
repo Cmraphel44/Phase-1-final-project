8eaae2ed156f117fb8e881288cefaf7910f3722e
Christopher <raphelchris@gmail.com>
2024-04-24 14:02:59 -0700

    finished array iteration

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ function startGame() {
   fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
     .then(response => response.json())
     .then(data => {
+      console.log(data);
       const cards = data.cards;
       humanCard.innerHTML = `<img src="${cards[0].image}" alt="${cards[0].value} of ${cards[0].suit}">`;
       computerCard.innerHTML = `<img src="${cards[1].image}" alt="${cards[1].value} of ${cards[1].suit}">`;
@@ -40,27 +41,71 @@ function startGame() {
     });
 }
 
+let winners = [];
+
 function checkWinner(card1, card2) {
   const humanValue = getValue(card1.value);
   const computerValue = getValue(card2.value);
+  
 
   if (humanValue > computerValue) {
     result.textContent = 'Congrats Human, you win!';
     humanScore++;
+    winners.push('Human')
   } else if (humanValue < computerValue) {
     result.textContent = 'Uh oh, Computer wins!';
     computerScore++;
+    winners.push('Computer');
   } else {
     result.textContent = 'It\'s a tie!';
   }
 
-  updateScores();
+  updateScores(winners);
 
   if (humanScore < 10 && computerScore < 10) {
     startButton.disabled = false;
   } else {
     endGame();
   }
+}
+
+function endGame() {
+  if (humanScore === 10) {
+    alert('HUMAN WINS! You should be proud. Go get yourself some ice cream!');
+    resetScoreboard();
+  } else if (computerScore === 10) {
+    alert('YOU LOSE. There\'s always next time!')
+    resetScoreboard();
+  }
+}
+
+function resetScoreboard() {
+  humanScore = 0;
+  computerScore = 0;
+  winners = [];
+  updateScores(winners);
+}
+
+function updateScores(winnersArray) {
+  console.log(winnersArray);
+  humanScoreElement.textContent = `Human: ${humanScore}`;
+  computerScoreElement.textContent = `Computer: ${computerScore}`;
+  const winnersList = document.getElementById('winners-list');
+  winnersList.innerHTML = "";
+  winnersArray.forEach(winner => {
+    console.log(winner);
+    const li = document.createElement('li');
+    li.innerHTML = winner;
+    winnersList.appendChild(li);
+  })
+}
+
+function getValue(cardValue) {
+  if (cardValue === 'KING') return 13;
+  if (cardValue === 'QUEEN') return 12;
+  if (cardValue === 'JACK') return 11;
+  if (cardValue === 'ACE') return 14;
+  return parseInt(cardValue);
 }
 
 
@@ -81,31 +126,5 @@ startButton.addEventListener("mouseout", function(event) {
 });
 
 
-function endGame() {
-  if (humanScore === 10) {
-    alert('HUMAN WINS! You should be proud. Go get yourself some ice cream!');
-    resetScoreboard();
-  } else if (computerScore === 10) {
-    alert('YOU LOSE. There\'s always next time!')
-    resetScoreboard();
-  }
-}
+//for my array iteration I want to take each winner element in an array
 
-function resetScoreboard() {
-  humanScore = 0;
-  computerScore = 0;
-  updateScores();
-}
-
-function updateScores() {
-  humanScoreElement.textContent = `Human: ${humanScore}`;
-  computerScoreElement.textContent = `Computer: ${computerScore}`;
-}
-
-function getValue(cardValue) {
-  if (cardValue === 'KING') return 13;
-  if (cardValue === 'QUEEN') return 12;
-  if (cardValue === 'JACK') return 11;
-  if (cardValue === 'ACE') return 14;
-  return parseInt(cardValue);
-}
